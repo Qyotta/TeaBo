7cd7a313b838dcb1ed8ef59948808061a1217857
christian-bromann <github@christian-bromann.com>
2011-10-25 15:57:07 +0200

    #3 markup login form

--- OnlineCollaborationPlatform/js/script.js
+++ OnlineCollaborationPlatform/js/script.js
@@ -3,6 +3,21 @@
  * @requires jQuery 1.6.2 or higher
  */
 
+$('nav.rightNavigation').hover(function() {
+    $(this).stop(true, false).animate({ right: 0 }, 500);
+}, function() {
+    $(this).stop(true, false).animate({ right: '-260px' }, 500);
+})
+
+var layer = 0;
+$('nav.rightNavigation ul li a').click(function() {
+    var hide = $($('nav.rightNavigation div div')[layer]);
+    var show = $('nav.rightNavigation div div.'+$(this).attr('href').substr(1));
+    hide.fadeOut(200, function() {
+        show.fadeIn(200);
+        layer = show.index();
+    });
+})
 
 
 
--- OnlineCollaborationPlatform/protected/views/layouts/main.php
+++ OnlineCollaborationPlatform/protected/views/layouts/main.php
@@ -22,10 +22,26 @@
 
     <nav class="rightNavigation">
         <ul>
-            <li><a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/user.png" alt="Login" title="Login" /></a></li>
-            <li><a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/info.png" alt="Info" title="Info" /></a></li>
-            <li><a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/wrench.png" alt="Settings" title="Settings" /></a></li>
+            <li><a href="#login"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/user.png" alt="Login" title="Login" /></a></li>
+            <li><a href="#info"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/info.png" alt="Info" title="Info" /></a></li>
+            <li><a href="#settings"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/icons/wrench.png" alt="Settings" title="Settings" /></a></li>
         </ul>
+       
+        <div>
+            <div class="login">
+                <form id="login-form" action="index.php" method="post">
+                <input type="text" name="LoginForm[username]" placeholder="your username" /><?php //echo $form->textField($model,'username'); ?>
+                <input type="password" name="LoginForm[password]" placeholder="your password" /><?php //echo $form->passwordField($model,'password'); ?>
+                <input type="submit" name="yt0" value="Login" class="submit" /><?php //echo CHtml::submitButton('Login'); ?>
+                </form>
+            </div>
+            <div class="info">
+                some info text
+            </div>
+            <div class="settings">
+                some settings here
+            </div>
+        </div>
     </nav>
 
     <nav class="bottomNavigation">
