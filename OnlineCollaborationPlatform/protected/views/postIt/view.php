798c92ecc32d0e3a895b4d21df608942bc1f14a5
Benjamin Kuhn <b.kuhn@hotmail.de>
2011-11-08 17:18:57 +0100

    #134 #136

--- OnlineCollaborationPlatform/protected/views/postIt/view.php
+++ OnlineCollaborationPlatform/protected/views/postIt/view.php
@@ -1,3 +1,15 @@
-<div class="postIt" style="position:absolute; left:<?= $postit->xposition?>px; top:<?= $postit->yposition?>px;"> 
-    <p> <?php echo $postit->text;?></p>
+<div class="postIt" style="position:absolute; left:<?= $postit->xposition?>px; top:<?= $postit->yposition?>px; z-index=<?= $postit->id?>"> 
+
+<?php $form = $this->beginWidget('CActiveForm', array(
+    'id'=>'user-form',
+    'enableAjaxValidation'=>true,
+    'focus'=>array($postit,'text'),
+)); 
+
+echo $form->textArea($postit, 'text');
+
+$this->endWidget();
+?>
+
+    
 </div>
--- OnlineCollaborationPlatform/protected/views/whiteboard/view.php
+++ OnlineCollaborationPlatform/protected/views/whiteboard/view.php
@@ -1,8 +1,10 @@
 <?php $this->pageTitle=Yii::app()->name.' | Whiteboard('.$whiteboard->name.')'; ?>
 
 <?php
+	// loads all postits for current whiteboard
     $postits = PostIt::model()->findAllByAttributes(array('whiteboardId'=> $whiteboard->id));
     foreach ($postits as $postit) {
+    	// shows a postit (/views/postIt/view.php)
 		$this->renderPartial('//postIt/view',array('postit'=>$postit));	
 	}
 ?>
