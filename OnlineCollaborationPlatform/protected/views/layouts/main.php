2eae6d45682691d55aecce0583c9df3f8f9e2a09
Andreas Sattler <info@royal-scripts.de>
2011-11-09 19:50:01 +0100

    - moved insert of jquery to head

--- OnlineCollaborationPlatform/protected/views/layouts/main.php
+++ OnlineCollaborationPlatform/protected/views/layouts/main.php
@@ -13,6 +13,9 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 	<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/libs/modernizr-2.0.6.min.js"></script>
 </head>
 
@@ -27,10 +30,7 @@
         $this->renderPartial('//modules/bottomNavigation');
     }
     ?>
-  
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+ 
     <script defer src="<?php echo Yii::app()->request->baseUrl; ?>/js/plugins.js"></script>
     <script defer src="<?php echo Yii::app()->request->baseUrl; ?>/js/script.js"></script>
     <script>
