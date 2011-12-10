3430f808a61ea9d62b90d9a77b7b485d69f6b68e
Andreas Sattler <info@royal-scripts.de>
2011-12-10 18:20:09 +0100

    fixed writing to the Database

--- de.bht.swp.lao.ocp/src/de/bht/swp/lao/ocp/user/UserDaoImpl.java
+++ de.bht.swp.lao.ocp/src/de/bht/swp/lao/ocp/user/UserDaoImpl.java
@@ -6,10 +6,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
 public class UserDaoImpl implements UserDao {
 
 	@PersistenceContext
