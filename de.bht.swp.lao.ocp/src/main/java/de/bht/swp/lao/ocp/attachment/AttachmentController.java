732e2a567bfbb3e138f579e75cc86c4608d9f38a
Andreas Sattler <info@royal-scripts.de>
2012-01-16 22:46:22 +0100

    - added apache-tika package for header analysis - added serverside-validation for filetypes

--- de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/attachment/AttachmentController.java
+++ de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/attachment/AttachmentController.java
@@ -1,7 +1,11 @@
 package de.bht.swp.lao.ocp.attachment;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +18,53 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+
 import de.bht.swp.lao.ocp.user.User;
 import de.bht.swp.lao.ocp.whiteboarditem.IWhiteboardItemDao;
 
 @Controller
 @RequestMapping(value="/attachment/*")
-public class AttachmentController {
+public class AttachmentController{
 	@Inject
 	private IWhiteboardItemDao<Attachment> attachmentDao;
 	
 	@RequestMapping(value="/uploadfile-{whiteboardId}.htm", method = RequestMethod.POST)
-	public void uploadFile(@RequestParam("data") MultipartFile data, @RequestParam("id") Long id, MultipartHttpServletRequest request,@PathVariable("whiteboardId")Long whiteboardId,HttpServletResponse response) throws IOException{
+	public void uploadFile(@RequestParam("data") MultipartFile data, @RequestParam("id") Long id, MultipartHttpServletRequest request,@PathVariable("whiteboardId")Long whiteboardId,HttpServletResponse response) throws IOException, Exception{
 		Attachment attachment = attachmentDao.findById(id);
 		User user = (User)request.getSession().getAttribute("user");
 		
+		// allowed file types:
+		String allowedMimeTypes[] = {"application/pdf","application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-powerpoint", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.oasis.opendocument.text", "application/x-vnd.oasis.opendocument.text", "application/vnd.oasis.opendocument.presentation", "application/x-vnd.oasis.opendocument.presentation", "application/vnd.oasis.opendocument.formula", "application/x-vnd.oasis.opendocument.formula"};
+		List<String> mimeTypes = Arrays.asList(allowedMimeTypes);
+		ByteArrayInputStream bStream = new ByteArrayInputStream(data.getBytes());
+		try {
+		 ContentHandler contenthandler = new BodyContentHandler();
+	      Metadata metadata = new Metadata();
+	      Parser parser = new AutoDetectParser();
+	      parser.parse(bStream, contenthandler, metadata);
+	      
+	      //getting MimeType
+	      if(!mimeTypes.contains(metadata.get(Metadata.CONTENT_TYPE))){ 
+	    	  System.out.println("");
+	    	  System.out.println("");
+	    	  System.out.println("");
+	    	  System.out.println("");
+	    	  System.out.println("");
+	    	  System.out.println("Not allowed Mime-Type: " + metadata.get(Metadata.CONTENT_TYPE));
+	    	  System.out.println("");
+	    	  System.out.println("");
+	    	  System.out.println("");
+	    	  System.out.println("");
+	    	  System.out.println("");
+	      }
+		}catch (Exception e) {
+	          e.printStackTrace();
+	        }
 		attachment.setData(data.getBytes());
 		attachment.setFilename(data.getOriginalFilename());
 		attachment.setCreator(user);
