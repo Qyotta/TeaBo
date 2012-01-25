01335951cd695fdf1772e1b5f935566acaafa6c8
christian-bromann <github@christian-bromann.com>
2012-01-25 14:09:57 +0100

    US #19 - as a user i want to logout - updated readme file - task #447 - #449 and #451

--- de.bht.swp.lao.ocp/src/main/webapp/js/application.js
+++ de.bht.swp.lao.ocp/src/main/webapp/js/application.js
@@ -17,27 +17,4 @@ $(function() {
         var dir = $(this).parent().css('right') === "0px";
         $(this).parent().animate({right: dir?"-199px":"0px"}, 200); 
     });
-    
-    $('#logout-dialog').dialog({
-        modal : true,
-        autoOpen : false,
-        closeOnEscape: false,
-        open: function(event, ui) { $(".ui-dialog-titlebar-close").hide(); },
-        open: function(event, ui) { $(".ui-dialog-titlebar").hide(); },
-        width : 420,
-        resizable: false,
-        draggable: false
-    });
-    
-    $('a[href="logout"]').live('click',  function(e) {
-        $('#logout-dialog').dialog('open');
-        $('#logout-dialog').css('min-height', '40px');
-        $('#logout-dialog').css('height', 'auto');
-        return false;
-    }); 
-    
-    $('#logout-dialog button.cancel').click(function(){
-        $('#logout-dialog input[type=text]').val("");
-        $('#logout-dialog').dialog('close');
-    });
 });
