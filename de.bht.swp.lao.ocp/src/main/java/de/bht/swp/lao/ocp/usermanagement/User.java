303b0a1b0ff8c6f294ee8ba2a5246d51d82a59d2
Benjamin Kuhn <b.kuhn@hotmail.de>
2012-01-27 16:33:30 +0100

    fixes #410 #473

--- de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/usermanagement/User.java
+++ de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/usermanagement/User.java
@@ -141,7 +141,12 @@ public class User {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + (id == null ? 0 : id.hashCode());
+
+        int hashid = 0;
+        if (id == null) {
+            hashid = id.hashCode();
+        }
+        result = prime * result + hashid;
         return result;
     }
 
