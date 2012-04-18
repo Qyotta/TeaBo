e92cbca17516ef5cf6667854ab4eb25a7915c132
christian-bromann <github@christian-bromann.com>
2012-04-18 17:54:24 +0200

    created select area - closes #1164

--- de.bht.swp.lao.ocp/src/main/webapp/resources/js/core/views/whiteboard.js
+++ de.bht.swp.lao.ocp/src/main/webapp/resources/js/core/views/whiteboard.js
@@ -8,16 +8,60 @@ define([
     var WhiteboardView = Backbone.View.extend({
         el: $("#page"),
         events:{
-            //'click .mainPanel input[type=submit]' : 'submitClicked',
+            'mousedown #whiteboard' : 'startSelection',
+            'mousemove #whiteboard' : 'dragEnterEvent',
+            'mouseup #whiteboard'   : 'dragEndEvent'
         },
         initialize:function(){
-			this.render();
+            this.render();
         },
-		render:function(){
-			var data = { _: _ };
+        render:function(){
+            var data = { _: _ };
             var compiledTemplate = _.template( whiteboardTemplate, data );
             this.el.html(compiledTemplate);
-		}
+        },
+        startSelection: function(e) {
+            if(this.isSelectionBox) return;
+            
+            this.isSelectionBox = true;
+            this.startDragX = e.clientX;
+            this.startDragY = e.clientY;
+            
+            this.selectionBox = $('<div />').
+                                    attr('id','selectionBox').
+                                    css('top',this.startDragY).
+                                    css('left',this.startDragX),
+                   whiteboard = $('#whiteboard');
+            
+            
+            whiteboard.append(this.selectionBox);
+        },
+        dragEnterEvent: function(e) {
+            if(!this.isSelectionBox) return;
+            
+            if(e.clientX - this.startDragX < 0) {
+                // drag from right to left
+                this.selectionBox.css('left',e.clientX);
+                this.selectionBox.css('width',this.startDragX - e.clientX);
+            } else {
+                // drag from left to right
+                this.selectionBox.css('width',e.clientX - this.startDragX);
+            }
+            if(e.clientY - this.startDragY < 0) {
+                // drag from bottom to top
+                this.selectionBox.css('top',e.clientY);
+                this.selectionBox.css('height',this.startDragY - e.clientY);
+            } else {
+                // drag from top to bottom
+                this.selectionBox.css('height',e.clientY - this.startDragY);
+            }
+        },
+        dragEndEvent: function() {
+            if(!this.isSelectionBox) return;
+            
+            this.selectionBox.remove();
+            this.isSelectionBox = false;
+        }
     });
     
     return WhiteboardView;
