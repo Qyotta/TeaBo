4a9cac3c8c71dfcdbee5cd0006a6c322a8125310
Andreas Sattler <info@royal-scripts.de>
2012-04-25 18:56:01 +0200

    closes #1223 added blureffect in whiteboard, mainpanel is not handled

--- de.bht.swp.lao.ocp/src/main/webapp/resources/js/core/views/dialogs/dialog.js
+++ de.bht.swp.lao.ocp/src/main/webapp/resources/js/core/views/dialogs/dialog.js
@@ -19,18 +19,18 @@ define([
             this.containerFadeOut();
         },
         containerFadeIn:function(elem) {
+            $('.whiteboard').draggable('disable').css('cursor', 'default!important').css('background', 'url("resources/images/whiteboard-background-blured.gif")');
+            $('.whiteboard *').draggable('disable').addClass('blurBox');
+            $('.whiteboard textarea, .whiteboard input').addClass('blurTextarea').attr('readonly', 'readonly').css('cursor', 'default');
+            $('.whiteboard textarea, .whiteboard .creator').addClass('blurText');
             this.el.fadeIn(500);
-            //$('.whiteboard').draggable('disable').css('cursor', 'default!important').css('background', 'url("../images/whiteboard-background-blured.gif")');
-            //$('.whiteboard .note, .whiteboard .attachment').draggable('disable').addClass('blurBox');
-            //$('.whiteboard .noteItems textarea').addClass('blurTextarea').attr('readonly', 'readonly').css('cursor', 'default');
-            //$('.whiteboard .noteItems textarea, .whiteboard .creator').addClass('blurText');
         },
         containerFadeOut:function(elem) {
             this.el.fadeOut();
-            //$('.whiteboard').draggable('enable').css('cursor', 'inherit').css('background', 'url("../images/whiteboard-background.gif")');
-            //$('.whiteboard .note, .whiteboard .attachment').draggable('enable').removeClass('blurBox');
-            //$('.whiteboard .noteItems textarea').removeClass('blurTextarea').removeAttr('readonly').css('cursor', 'inherit');
-            //$('.whiteboard .noteItems textarea, .whiteboard .creator').removeClass('blurText');
+            $('.whiteboard').draggable('enable').css('cursor', 'inherit').css('background', 'url("resources/images/whiteboard-background.gif")');
+            $('.whiteboard *').draggable('enable').removeClass('blurBox');
+            $('.whiteboard * textarea').removeClass('blurTextarea').removeAttr('readonly').css('cursor', 'inherit');
+            $('.whiteboard * textarea, .whiteboard .creator').removeClass('blurText');
         }
     });
     
