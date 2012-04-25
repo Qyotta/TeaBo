ebe9b96b11417f6c6da105d96c6ce3a2c2cb9a91
Andreas Sattler <info@royal-scripts.de>
2012-04-25 15:53:40 +0200

    closes #1219 migrated z-index ordering from master, currently only for notes

--- de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/note/NoteDTO.java
+++ de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/note/NoteDTO.java
@@ -2,68 +2,78 @@ package de.bht.swp.lao.ocp.note;
 
 public class NoteDTO {
 
-    private String creator;
-    private Long id;
-    private String text;
-    private Long x;
-    private Long y;
-    private Long whiteboardId;
+  private String creator;
+  private Long id;
+  private String text;
+  private Long x;
+  private Long y;
+  private Long whiteboardId;
+  private Integer orderIndex;
 
-    public NoteDTO(Note note) {
-        this.setCreator(note.getCreator().getEmail());
-        this.setId(note.getId());
-        this.setText(note.getText());
-        this.setWhiteboardId(note.getWhiteboard().getId());
-        this.setX(note.getX());
-        this.setY(note.getY());
-    }
+  public NoteDTO(Note note) {
+    this.setCreator(note.getCreator().getEmail());
+    this.setId(note.getId());
+    this.setText(note.getText());
+    this.setWhiteboardId(note.getWhiteboard().getId());
+    this.setX(note.getX());
+    this.setY(note.getY());
+    this.setOrderIndex(note.getOrderIndex());
+  }
 
-    public String getCreator() {
-        return creator;
-    }
+  public String getCreator() {
+    return creator;
+  }
 
-    public void setCreator(String creator) {
-        this.creator = creator;
-    }
+  public void setCreator(String creator) {
+    this.creator = creator;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getText() {
-        return text;
-    }
+  public String getText() {
+    return text;
+  }
 
-    public void setText(String text) {
-        this.text = text;
-    }
+  public void setText(String text) {
+    this.text = text;
+  }
 
-    public Long getWhiteboardId() {
-        return whiteboardId;
-    }
+  public Long getWhiteboardId() {
+    return whiteboardId;
+  }
 
-    public void setWhiteboardId(Long whiteboardId) {
-        this.whiteboardId = whiteboardId;
-    }
+  public void setWhiteboardId(Long whiteboardId) {
+    this.whiteboardId = whiteboardId;
+  }
 
-    public Long getX() {
-        return x;
-    }
+  public Long getX() {
+    return x;
+  }
 
-    public void setX(Long x) {
-        this.x = x;
-    }
+  public void setX(Long x) {
+    this.x = x;
+  }
 
-    public Long getY() {
-        return y;
-    }
+  public Long getY() {
+    return y;
+  }
 
-    public void setY(Long y) {
-        this.y = y;
-    }
+  public void setY(Long y) {
+    this.y = y;
+  }
+
+  public Integer getOrderIndex() {
+    return orderIndex;
+  }
+
+  public void setOrderIndex(Integer orderIndex) {
+    this.orderIndex = orderIndex;
+  }
 
 }
