f7803ef09d76e0ae02cf130744d843d854c1ae10
Benjamin Kuhn <b.kuhn@hotmail.de>
2012-04-28 21:44:22 +0200

    closes #1261 -only send whiteboard name to server, not x and y

--- de.bht.swp.lao.ocp/src/main/webapp/resources/js/core/models/whiteboard.js
+++ de.bht.swp.lao.ocp/src/main/webapp/resources/js/core/models/whiteboard.js
@@ -4,8 +4,13 @@ define([
 ], function(_, Backbone) {
     var Whiteboard = Backbone.Model.extend({
         defaults: {
-            name: null
+            name: null,
+            x:0,
+            y:0
         },
+        toJSON : function(){
+    	  return {name: this.get("name")};
+    	}
     });
     
     return Whiteboard;
