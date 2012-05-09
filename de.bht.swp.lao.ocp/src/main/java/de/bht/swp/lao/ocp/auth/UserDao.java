42a2da92e03cee16dd80c49d5e9fd5dae4de5600
Andreas Sattler <info@royal-scripts.de>
2012-05-09 14:14:24 +0200

    fixes #1316

--- de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/auth/UserDao.java
+++ de.bht.swp.lao.ocp/src/main/java/de/bht/swp/lao/ocp/auth/UserDao.java
@@ -10,38 +10,38 @@ import org.springframework.transaction.annotation.Transactional;
 
 public class UserDao implements IUserDao {
 
-    @PersistenceContext
-    private EntityManager em;
+  @PersistenceContext
+  private EntityManager em;
 
-    @Override
-    public User findById(Integer id) {
-        return em.find(User.class, id);
-    }
+  @Override
+  public User findById(Integer id) {
+    return em.find(User.class, id);
+  }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public List<User> findAll() {
-        return em.createQuery("from User u").getResultList();
-    }
+  @SuppressWarnings("unchecked")
+  @Override
+  public List<User> findAll() {
+    return em.createQuery("from User u").getResultList();
+  }
 
-    @Override
-    public User findByEmail(String email) {
-        try {
-            return (User) em.createQuery("from User u where u.email = ?1")
-                    .setParameter(1, email).setMaxResults(1).getSingleResult();
-        } catch (NoResultException e) {
-            return null;
-        }
+  @Override
+  public User findByEmail(String email) {
+    try {
+      return (User) em.createQuery("from User u where u.email = ?1").setParameter(1, email).setMaxResults(1)
+          .getSingleResult();
+    } catch (NoResultException e) {
+      return null;
     }
+  }
 
-    @Override
-    @Transactional
-    public void save(User user) {
-        if (user != null) {
-            em.merge(user);
-        } else {
-            em.persist(user);
-        }
+  @Override
+  @Transactional
+  public void save(User user) {
+    if (user.getId() != null) {
+      em.merge(user);
+    } else {
+      em.persist(user);
     }
+  }
 
 }
