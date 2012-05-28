ff6fc57429773a36d580134d7d20228f49c0f412
Benjamin Kuhn <b.kuhn@hotmail.de>
2012-05-28 21:58:38 +0200

    refs #1591 whiteboard item module bug fix

--- modules/whiteboard-item/module.js
+++ modules/whiteboard-item/module.js
@@ -6,9 +6,9 @@ exports.init = function() {
     
 }
 
-exports.comet = {
+exports.comet = [
         { channel : '/service/whiteboardItem/move', callback : function(message){}},
         { channel : '/service/whiteboardItem/delete', callback : function(message){}},
         { channel : '/service/whiteboardItem/editing', callback : function(message){}},
         { channel : '/service/whiteboardItem/order', callback : function(message){}}     
-}
+]
