74c15c09c191aa2d275626bf713d626712eac282
Andreas Sattler <info@royal-scripts.de>
2012-06-12 22:07:56 +0200

    refs #1596

--- modules/attachment/public/js/views/confirm_delete.js
+++ modules/attachment/public/js/views/confirm_delete.js
@@ -1,54 +1,57 @@
-define([ 
-    'jquery', 
-    'underscore', 
-    'backbone', 
+define([
+    'jquery',
+    'underscore',
+    'backbone',
     '/core/js/views/dialogs/dialog.js',
-    'text!/attachment/templates/confirm_delete.html' 
-    ], function($, _, Backbone, Dialog, confirmDeleteTemplate) {
-    var ConfirmDeleteView = Dialog
-            .extend({
-                showDialogFlag:"DeleteConfirmFlag",
-                initialize : function() {
-                    _.bindAll(this, 'showConfirmDialog', 'confirmed', 'hideConfirmDialog', 'shouldShowDialog');
-                    window.app.eventDispatcher.bind("attachment:delete_clicked",this.showConfirmDialog);
-                },
-                events : {
-                    'click button.cancel' : 'hideConfirmDialog',
-                    'click input[type=submit]' : 'confirmed'
-                },
-                render : function() {
-                    var compiledTemplate = _.template(confirmDeleteTemplate);
-                    $(this.el).attr('id','confirmDeleteNoteContainer');
-                    $(this.el).html(compiledTemplate);
-                    $('#dialogs').html(this.el);
-                },
-                showConfirmDialog : function(model) {
-                    this.model = model;
-                    if(this.shouldShowDialog()){
-                        this.showDialog();
-                    } else {
-                        window.app.eventDispatcher.trigger('attachment:delete',this.model);
-                    }
-                },
-                shouldShowDialog : function(){
-                    if(typeof window.app.user.get("settings").where(this.showDialogFlag)[0] == "undefined" || window.app.user.get("settings").where(this.showDialogFlag)[0].get("value") == "true" || window.app.user.get("settings").where(this.showDialogFlag)[0].get("value") == true){
-                        return true;
-                    } else {
-                        return false;
-                    }   
-                },
-                hideConfirmDialog : function(evt) {
-                    evt.preventDefault();
-                    this.hideDialog();
-                    this.model = null;
-                },
-                confirmed : function(evt) {
-                    evt.preventDefault();
-                    window.app.modules.userSettings.set(this.showDialogFlag,!$('#dialogs :checkbox').is(':checked'));
-                    this.hideDialog();
-                    window.app.eventDispatcher.trigger('attachment:delete',this.model);
-                }
-            });
-
+    'text!/attachment/templates/confirm_delete.html'
+], function($, _, Backbone, Dialog, confirmDeleteTemplate){
+   
+    var ConfirmDeleteView = Dialog.extend({
+        showDialogFlag:"DeleteConfirmFlag",
+        initialize:function(){
+            _.bindAll(this,'showConfirmDialog', 'confirmed', 'hideConfirmDialog', 'shouldShowDialog');
+            window.app.eventDispatcher.bind("attachment:delete_clicked",this.showConfirmDialog);
+        },
+        events:{
+            'click #confirmDeleteNoteContainer button.cancel' : 'hideConfirmDialog',
+            'click #confirmDeleteNoteContainer input[type=submit]': 'confirmed'
+        },
+        render: function(){
+            var compiledTemplate = _.template(confirmDeleteTemplate);
+            $(this.el).attr('id','confirmDeleteNoteContainer');
+            $(this.el).html(compiledTemplate);
+            $('#dialogs').html(this.el);
+        },
+        showConfirmDialog:function(model){
+            this.model = model;
+            if(this.shouldShowDialog()){
+                this.showDialog();
+            }
+            else {
+                window.app.eventDispatcher.trigger('attachment:delete',this.model);
+            }
+        },
+        shouldShowDialog : function(){
+//            if(typeof window.app.user.get("settings").where(this.showDialogFlag)[0] == "undefined" || window.app.user.get("settings").where(this.showDialogFlag)[0].get("value") == "true" || window.app.user.get("settings").where(this.showDialogFlag)[0].get("value") == true){
+//                return true;
+//            } else {
+//                return false;
+//            }   
+            return true;
+        },
+        hideConfirmDialog:function(evt){
+            evt.preventDefault();
+            this.hideDialog();
+            this.model = null;
+        },
+        confirmed:function(evt){
+            evt.preventDefault();
+//            window.app.modules.userSettings.set(this.showDialogFlag,!$('#dialogs :checkbox').is(':checked'));
+            this.hideDialog();
+            window.app.eventDispatcher.trigger('attachment:delete',this.model);
+            window.app.eventDispatcher.trigger('whiteboardItem:delete',this.model);
+        }
+    });    
+    
     return ConfirmDeleteView;
 });
