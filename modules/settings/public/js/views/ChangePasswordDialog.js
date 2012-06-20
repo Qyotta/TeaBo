576c097c4969f344ebf54d4dd3bcbc0dc5e6ebe8
Andreas Sattler <info@royal-scripts.de>
2012-06-20 17:58:50 +0200

    closes #1843 closes #1844 added dialog + form for change password

---
+++ modules/settings/public/js/views/ChangePasswordDialog.js
@@ -0,0 +1,39 @@
+define([
+    'jquery', 
+    'underscore', 
+    'backbone', 
+    '/core/js/utils/model_command.js',
+    '/core/js/views/dialogs/dialog.js',
+    'text!/settings/templates/changePasswordDialog.html'
+], function($, _, Backbone, ModelCommand, Dialog, changePasswordDialogTemplate) {
+    var ChangePasswordDialog = Dialog.extend({
+        initialize : function(options) {
+            _.bindAll(this, 'showChangePasswordDialog');
+            window.app.eventDispatcher.bind("settingsMenu:changePassword", this.showChangePasswordDialog);
+        },
+        events : {
+            'click button.cancel' : 'canceled',
+            'click input[type=submit]': 'submited'
+        },
+        submited:function(){
+            
+        },
+        canceled:function(e){
+            e.preventDefault();
+            this.hideDialog();
+        },
+        render : function() {
+            var data = {};
+            var compiledTemplate = _.template(changePasswordDialogTemplate, data);
+            $(this.el).html(compiledTemplate);
+            $('#dialogs').html(this.el);
+            this.delegateEvents();
+        },
+        showChangePasswordDialog : function() {
+            console.log("show change password dialog");
+            this.showDialog();
+        }
+    });
+
+    return ChangePasswordDialog;
+});
