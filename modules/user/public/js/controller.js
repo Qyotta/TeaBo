5152f20dfd9249935531d652a02d840dcb54e80c
christian-bromann <contact@christian-bromann.com>
2012-06-20 11:59:45 +0200

    moved logout dialog to user module - refs #1836

--- modules/user/public/js/controller.js
+++ modules/user/public/js/controller.js
@@ -1,7 +1,13 @@
-define([], function(){
+define([
+    '/user/js/views/logoutDialog.js'
+], function(LogoutDialogView){
     
     var UserController = function(options){
-        
+
+        if(!window.app.logoutDialogView){
+            window.app.logoutDialogView = new LogoutDialogView();
+        }
+
     };
     
     return UserController;
