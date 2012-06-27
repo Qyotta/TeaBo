1428236c178d487744ee0797b68b852b20dba087
Benjamin Kuhn <b.kuhn@hotmail.de>
2012-06-27 17:27:10 +0200

    refs #1956,#1839 assignment user changed when user preferences changed

--- modules/assignment/public/js/model/assignment.js
+++ modules/assignment/public/js/model/assignment.js
@@ -10,7 +10,7 @@ define([
         },
         set: function(attributes, options) {
             if (attributes.user !== undefined && !(attributes.user instanceof User)) {
-                attributes.user = new User(attributes.user[0]);
+                attributes.user = new User(attributes.user);
             }
             return Backbone.Model.prototype.set.call(this, attributes, options);
         },
