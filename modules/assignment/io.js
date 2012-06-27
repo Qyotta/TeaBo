6a153713601d7c2418629e219bef9848c20238e5
Benjamin Kuhn <b.kuhn@hotmail.de>
2012-06-27 13:47:46 +0200

    refs #1944 push color changes via comet

--- modules/assignment/io.js
+++ modules/assignment/io.js
@@ -9,4 +9,14 @@ io['/service/assignment/changeOnlineStatus'] = function(bayeux,channel,obj) {
     })
 }
 
+io['/service/assignment/changeColor'] = function(bayeux,channel,obj) {
+    Assignment.findOne({_id:obj.assignmentId}, function(err,foundAssignment) {
+        foundAssignment.color = [obj.color_r,obj.color_g,obj.color_b];
+        foundAssignment.save();
+        bayeux.getClient().publish('/assignment/change/color/'+foundAssignment.whiteboard[0]._id, 
+            { id:foundAssignment._id, color_r:obj.color_r,color_g:obj.color_g,color_b:obj.color_b}
+        );
+    })
+}
+
 exports.io = io;
